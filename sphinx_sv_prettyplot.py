ddc490e8ed8d286842679eb5a6e9aa4d4b3fdd16
Francesco Conti <f.conti@unibo.it>
2019-03-06 23:47:13 +0100

    fixes

--- sphinx_sv_prettyplot.py
+++ sphinx_sv_prettyplot.py
@@ -1,13 +1,14 @@
 from docutils import nodes
 from docutils.parsers.rst import Directive
 import os
+import prettyplot
 
 class SVPrettyPlot(Directive):
 
     required_arguments = 1
     optional_arguments = 1
 
-    def run(self, path, genimg_path='genimg'):
+    def run(self, path, genimg_path='_build/genimg'):
         basename = os.path.basename(path)
         s = ".. _%s:\n" % basename
         s += ".. figure:: %s/%s.pdf\n" % (genimg_path, basename)
@@ -15,6 +16,11 @@ class SVPrettyPlot(Directive):
         s += ":width: 70%\n"
         s += ":align: center\n"
         paragraph_node = nodes.paragraph(text=s)
+        try:
+            os.makedirs(genimg_path)
+        except FileExistsError:
+            pass
+        prettyplot.sv_prettyplot(path, '%s/%s.pdf' % (genimg_path, basename))
         return [paragraph_node]
 
 def setup(app):
