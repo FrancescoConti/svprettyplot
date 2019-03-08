d5b6edfa4c7515c34c8173b7c651e93f06d78452
Francesco Conti <f.conti@unibo.it>
2019-03-08 23:41:01 +0100

    Works nicely!

--- sphinx_sv_prettyplot.py
+++ sphinx_sv_prettyplot.py
@@ -1,27 +1,50 @@
 from docutils import nodes
 from docutils.parsers.rst import Directive
 import os
-import prettyplot
+import svprettyplot.prettyplot as prettyplot
+from docutils.parsers.rst.directives.images import Image
+from sphinx.util.docutils import SphinxDirective
+from sphinx.ext.graphviz import figure_wrapper
+from sphinx.errors import SphinxError
+from docutils.parsers.rst import directives
 
-class SVPrettyPlot(Directive):
+class SVPrettyPlot(Image, SphinxDirective):
 
     required_arguments = 1
     optional_arguments = 1
+    final_argument_whitespace = False
 
-    def run(self, path, genimg_path='_build/genimg'):
+    option_spec = Image.option_spec.copy()
+    option_spec['caption'] = directives.unchanged
+    has_content = True
+
+    def run(self):
+        path = self.arguments[0]
+        try:
+            genimg_path = self.arguments[1] or '_build/genimg'
+        except IndexError:
+            genimg_path = '_build/genimg'
+
+        document = self.state.document
+
+        # Store code in a special docutils node and pick up at rendering
+        # node = svprettyplotnode()
+
         basename = os.path.basename(path)
-        s = ".. _%s:\n" % basename
-        s += ".. figure:: %s/%s.pdf\n" % (genimg_path, basename)
-        s += ":figwidth: 70%\n"
-        s += ":width: 70%\n"
-        s += ":align: center\n"
-        paragraph_node = nodes.paragraph(text=s)
+        inner_node = nodes.image(uri='%s/%s.pdf' % (genimg_path, basename), figwidth='95%', width='95%', align='center')
+
+        node = inner_node
+
         try:
             os.makedirs(genimg_path)
         except FileExistsError:
             pass
         prettyplot.sv_prettyplot(path, '%s/%s.pdf' % (genimg_path, basename))
-        return [paragraph_node]
+
+        self.arguments = ["dummy"]
+        (inner_node['image_node'],) = Image.run(self)
+
+        return [node]
 
 def setup(app):
     app.add_directive("svprettyplot", SVPrettyPlot)
